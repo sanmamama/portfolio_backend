de23bc7ec5e3e91cffd45a7cafcb73027a30d9f8
ajifly723 <ajifly723@gmail.com>
2024-06-12 11:40:58 +0900

    add
    add project
    rest_framework

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from .serializer import UserSerializer
+
+class UserViewSet(APIView):
+    def get(self, request):
+        articles = User.objects.all()
+        serializer = UserSerializer(articles, many=True)
+        return Response(serializer.data)

fd693d11c93dbcb37e060885b2fa90c8f76007a9
sanmamama <ajifly723@gmail.com>
2024-09-09 21:10:49 +0900

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -15,12 +15,18 @@ class PostAdminForm(forms.ModelForm):
 class PostAdmin(admin.ModelAdmin):
     form = PostAdminForm
 """
+admin.site.register(Mention)
+admin.site.register(Follow)
+admin.site.register(Like)
+admin.site.register(List)
+admin.site.register(ListMember)
+admin.site.register(Repost)
+admin.site.register(Notification)
 
 admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Message)
 admin.site.register(Blog, MarkdownxModelAdmin)
 admin.site.register(Tag)
-#admin.site.register(Comment)
 admin.site.register(Category)
 admin.site.register(Contact)

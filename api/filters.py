6d3bf15fea98b485bf5c56d9db361d4e191e9322
sanmamama <ajifly723@gmail.com>
2024-09-10 13:15:13 +0900

    update
    使用していない部分だけど検索文字列でフィルターかけられるようにアップデート

--- api/filters.py
+++ api/filters.py
@@ -1,6 +1,6 @@
 import django_filters
 from .models import Blog
-
+from django.db.models import Q
 
 #イカ、ブログ
 
@@ -8,10 +8,11 @@ class BlogFilter(django_filters.FilterSet):
     category = django_filters.CharFilter(field_name='category__name', lookup_expr='icontains')
     tag = django_filters.CharFilter(field_name='tag__name', lookup_expr='icontains')
     date = django_filters.CharFilter(method='filter_by_date')
+    q = django_filters.CharFilter(method='filter_by_all_fields')
 
     class Meta:
         model = Blog
-        fields = ['category','tag','date']
+        fields = ['category','tag','date','q']
 
     def filter_by_date(self, queryset, name, value):
         if len(value) == 6 and value.isdigit():
@@ -19,3 +20,11 @@ class BlogFilter(django_filters.FilterSet):
             month = int(value[4:6])
             return queryset.filter(created_at__year=year, created_at__month=month)
         return queryset
+    
+    def filter_by_all_fields(self, queryset, name, value):
+        return queryset.filter(
+            Q(category__name__icontains=value) |
+            Q(tag__name__icontains=value) |
+            Q(title__icontains=value) |
+            Q(content__icontains=value)
+        )

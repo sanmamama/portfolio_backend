99d90495408f313a9cf37823f8778283c98ecf24
sanmamama <144568399+sanmamama@users.noreply.github.com>
2026-09-22 13:03:04 +0900

    ハイライト対応

--- api/models.py
+++ api/models.py
@@ -302,7 +302,7 @@ class Blog(models.Model):
             update_fields.update({'content_html', 'toc_html'})
 
         md = markdown.Markdown(
-            extensions=['toc']
+            extensions=['toc', 'fenced_code']
         )
 
         # :::answer ～ ::: を <details> に変換
